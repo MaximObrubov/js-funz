886fb81dfb8c8b08698aa20223350be08eea6ca3
Maxim Obrubov <m.obrubov@yandex.ru>
2022-07-18 15:45:35 +0200

    Update inner-palindrome-detection.js

--- inner-palindrome-detection.js
+++ inner-palindrome-detection.js
@@ -1,36 +1,34 @@
 // NOTE: my ugly solution
 function longest_palindrome(s) {
-  const slen = s.length;
-  
-  const isPalindrome = (_i, len) => {
-    let lim = (_i + len) >> 1;
-    
-    for (let i = 0; i < lim; i++) {
-      const start = i + _i;
-      const end = len + _i - i - 1;
-      
-      if (s[start] !== s[end]) return false;
+  const isPalindrome = (left, right) => {
+    while(left < right) {
+      if (s[left] !== s[right]) return false;
+      left++;
+      right--;
     }
-
     return true;
   };
   
+  
+  
+  
   const len = s.length;
   let palindromeLength = s.length;
-  let max = "";
+  let [left, right] = [0, 0];
   
-  while (palindromeLength > max.length) {
+  while (palindromeLength > right - left) {
     let range = len - palindromeLength;
     
     for (let i = 0; i <= range; i++) {
-      if (isPalindrome(i, palindromeLength) && max.length < palindromeLength) {
-        max = s.substr(i, palindromeLength);
-      };
+      if (isPalindrome(i, i + palindromeLength - 1)) {
+        const [l, r] = [i, i + palindromeLength - 1];
+        if (r - l > right - left) [left, right] = [l, r]
+      }
     }
     palindromeLength--;
   }
-  
-  return max;
+    
+  return s.substring(left, right + 1);
 }
 
 
